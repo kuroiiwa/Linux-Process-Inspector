f8b347ff325640e89e65a880931da16bd22b3399
Matthew Chan <matthew.a.chan@gmail.com>
2018-10-02 00:40:16 -0400

    Remove print statement

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -11,34 +11,35 @@
 extern int errno;
 
 struct prinfo {
-	pid_t parent_pid;		/* process id of parent */
-	pid_t pid;			/* process id */
-	pid_t first_child_pid;  	/* pid of youngest child */
-	pid_t next_sibling_pid;  	/* pid of older sibling */
-	long state;			/* current state of process */
-	uid_t uid;			/* user id of process owner */
-	char comm[64];			/* name of program executed */
+  pid_t parent_pid;		/* process id of parent */
+  pid_t pid;			/* process id */
+  pid_t first_child_pid;  	/* pid of youngest child */
+  pid_t next_sibling_pid;  	/* pid of older sibling */
+  long state;			/* current state of process */
+  uid_t uid;			/* user id of process owner */
+  char comm[64];			/* name of program executed */
 };
 
 int main(int argc, char **argv)
 {
-	struct prinfo *buf;
-	int nr;
-	
-	buf = (struct prinfo *)malloc(sizeof(struct prinfo) * BUF_SIZE);
-	nr = BUF_SIZE;
-	long res = syscall(SYS_ptree, buf, &nr); 
+  struct prinfo *buf;
+  int nr;
 
-	if (res != 0) {
-		printf("Syscall failed with errno: %d\n", errno);
-		return 1;
-	}
+  buf = (struct prinfo *)malloc(sizeof(struct prinfo) * BUF_SIZE);
+  nr = BUF_SIZE;
+  long res = syscall(SYS_ptree, buf, &nr); 
 
-	printf("%d processes in total.\n", nr);
+  if (res != 0) {
+    printf("Syscall failed with errno: %d\n", errno);
+    return 1;
+  }
 
-	if (nr > BUF_SIZE) nr = BUF_SIZE;
-	for (int i = 0; i < nr; i++)
-		printf("%s,%d,%ld,%d,%d,%d,%u\n", buf[i].comm, buf[i].pid, buf[i].state, buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
-	free(buf);
-	return res;
+  /* printf("%d processes in total.\n", nr); */
+
+  if (nr > BUF_SIZE) nr = BUF_SIZE;
+  for (int i = 0; i < nr; i++)
+    printf("%s,%d,%ld,%d,%d,%d,%u\n", buf[i].comm, buf[i].pid, buf[i].state, buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
+
+  free(buf);
+  return res;
 }
