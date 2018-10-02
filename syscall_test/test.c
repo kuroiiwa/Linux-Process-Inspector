500768f5e50da23ac1d17365c7bd67cc4a696051
Matthew Chan <matthew.a.chan@gmail.com>
2018-10-01 22:32:19 -0400

    Add line to free malloc'd memory

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -3,9 +3,12 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define SYS_ptree 326
 #define BUF_SIZE 300
+
+extern int errno;
 
 struct prinfo {
 	pid_t parent_pid;		/* process id of parent */
@@ -25,13 +28,17 @@ int main(int argc, char **argv)
 	buf = (struct prinfo *)malloc(sizeof(struct prinfo) * BUF_SIZE);
 	nr = BUF_SIZE;
 	long res = syscall(SYS_ptree, buf, &nr); 
-/*	if (res != 0) {
-		printf("failed!\n");
+
+	if (res != 0) {
+		printf("Syscall failed with errno: %d\n", errno);
 		return 1;
-	}*/
+	}
+
 	printf("%d processes in total.\n", nr);
+
 	if (nr > BUF_SIZE) nr = BUF_SIZE;
 	for (int i = 0; i < nr; i++)
 		printf("%s,%d,%ld,%d,%d,%d,%u\n", buf[i].comm, buf[i].pid, buf[i].state, buf[i].parent_pid, buf[i].first_child_pid, buf[i].next_sibling_pid, buf[i].uid);
+	free(buf);
 	return res;
 }
