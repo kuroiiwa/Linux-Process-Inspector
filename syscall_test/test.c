374f618a5ead973379d210284b2953518c5da907
Zhicheng Wu <zhicheng.wu@columbia.edu>
2018-10-02 21:24:45 -0400

    add error handler when memory allocate error

--- syscall_test/test.c
+++ syscall_test/test.c
@@ -3,21 +3,18 @@
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 
 #define SYS_ptree 326
 #define BUF_SIZE 300
 
-extern int errno;
-
 struct prinfo {
-	pid_t parent_pid;		/* process id of parent */
-        pid_t pid;			/* process id */
-	pid_t first_child_pid;  	/* pid of youngest child */
-	pid_t next_sibling_pid;  	/* pid of older sibling */
-	long state;			/* current state of process */
-	uid_t uid;			/* user id of process owner */
-	char comm[64];			/* name of program executed */
+	pid_t parent_pid;
+	pid_t pid;
+	pid_t first_child_pid;
+	pid_t next_sibling_pid;
+	long state;
+	uid_t uid;
+	char comm[64];
 };
 
 int main(int argc, char **argv)
@@ -28,26 +25,30 @@ int main(int argc, char **argv)
 	int a[10000];
 
 	buf = (struct prinfo *)malloc(sizeof(struct prinfo) * BUF_SIZE);
+	if (buf == NULL)
+		printf("error: %s\n", "allocate error");
 	nr = BUF_SIZE;
-	long res = syscall(SYS_ptree, buf, &nr); 
+	long res = syscall(SYS_ptree, buf, &nr);
 	pid_t q;
 
-	if (nr > BUF_SIZE) nr = BUF_SIZE;
-	for (int i = 0; i < nr; i++){
-		p = buf[i];		
-		a[p.pid] = p.parent_pid;		
+	if (nr > BUF_SIZE)
+		nr = BUF_SIZE;
+	for (int i = 0; i < nr; i++) {
+		p = buf[i];
+		a[p.pid] = p.parent_pid;
 	}
-	for (int i = 0; i < nr; i++){
+	for (int i = 0; i < nr; i++) {
 		p = buf[i];
 		q = p.parent_pid;
-		while(q != 0) {
+		while (q != 0) {
 			q = a[q];
 			printf("        ");
 		}
-		if(p.pid != 0)	printf("        ");
-		printf("%s,%d,%ld,%d,%d,%d,%u\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+		if (p.pid != 0)
+			printf("        ");
+		printf("%s,%d,%ld,%d,%d,%d,%u\n", p.comm, p.pid, p.state,
+p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
 	}
-	printf("%ld", res);
 	free(buf);
 	return res;
 }
